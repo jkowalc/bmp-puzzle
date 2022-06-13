a06d69a8679737e59bb09392436f7f49f5a2372f
Jan Kowalczewski <official.jan.kk@gmail.com>
2022-06-13 03:26:31 +0200

    Make project

--- X86_32/src/main.c
+++ X86_32/src/main.c
@@ -2,22 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include "image.h"
+extern void spread_tiles(ImageInfo* imgInfo, u_int32_t n, u_int32_t m);
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
     char* ifname = "source.bmp";
     char* ofname = "result.bmp";
-    u_int32_t n = atoi(argv[1]);
-    u_int32_t m = atoi(argv[2]);
-    FILE* ifstream = fopen(ifname, "rb");
-    if(!ifstream)
+    u_int32_t n = 4;
+    u_int32_t m = 3;
+    ImageInfo* imgInfo = readBmp(ifname);
+    if(imgInfo == NULL)
     {
-        printf("Can't open source file\n");
-        return 0;
+        return 2;
     }
-
-    // read bmp
-    spread_tiles(n, m);
-    // save bmp
+    spread_tiles(imgInfo, n, m);
+    if(saveBmp(ofname, imgInfo) != 0)
+    {
+        printf("Error saving BMP");
+        return 2;
+    }
+    freeImage(imgInfo);
 };
