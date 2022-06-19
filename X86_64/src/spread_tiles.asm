d36b29540e3709335854cce8e789bb0300e8fca3
Jan Kowalczewski <official.jan.kk@gmail.com>
2022-06-19 19:38:52 +0200

    First prototype of the x86_64 version

--- X86_64/src/spread_tiles.asm
+++ X86_64/src/spread_tiles.asm
@@ -11,37 +11,34 @@ extern rand
 ; spread_tiles
 ; arguments:
 ; 	rdi - address of ImgInfo descriptor
-;	rsi - n parameter
-;	rdx - m parameter
+;	rsi - n parameter (32bit int)
+;	rdx - m parameter (32bit int)
 ; return:
 ; 	none
 spread_tiles:
-    push ebp
-    mov ebp, esp
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
 
-    push ebx
-    push esi
+    mov r12, rdi ; save ImgInfo address for exchange_puzzles
+    mov r13d, edx ; save m for exchange_puzzles
 
-    mov ecx, [ebp+8] ; load ImgInfo
-    push ecx ; push ImgInfo for exchange_puzzles
-
-    mov esi, [ebp+16] ; load m
-    mov eax, [ecx] ; load width
-    cmp esi, eax
+    mov eax, [rdi] ; load width
+    cmp edx, eax
     jg spread_tiles_exit ; if m > width exit (not allowed)
     xor edx, edx ; edx = 0
-    div esi ; width // m
-    push esi ; push m for exchange_puzzles
+    div r13d ; width // m
     lea eax, [eax + 2*eax] ; eax = 3 * (width // m)
-    push eax ; push width of one puzzle for exchange_puzzles
+    mov r14d, eax ; save width for exchange_puzzles
 
-    mov ebx, [ebp+12] ; load n
-    mov eax, [ecx+4] ; load height
-    cmp ebx, eax
+    mov eax, [rdi+4] ; load height
+    cmp esi, eax
     jg spread_tiles_exit ; if n > height exit (not allowed)
     xor edx, edx
-    div ebx ; height // n
-    push eax ; push height for exchange_puzzles
+    div esi ; height // n
+    mov r15d, eax ; save height for exchange_puzzles
 
 	; Fisher–Yates shuffle algorithm
 	; int j;
@@ -50,32 +47,35 @@ spread_tiles:
 	; 	j = RandIntRange(0, i - 1)
 	;	exchange_puzzles(i, j)
 	; }
-
+    mov ebx, r13d
     imul ebx, esi ; ebx - i
 
 spread_tiles_loop:
     call rand
     xor edx, edx
     div ebx ; edx = randint % i
-    push edx ; push second puzzle for exchange_puzzles
+    mov esi, edx ; num of second puzzle for exchange_puzzles
     dec ebx ; temp decrement ebx
-    push ebx ; push first puzzle for exchange_puzzles
+    mov edi, ebx ; num of first puzzle for exchange_puzzles
     inc ebx ; restore ebx
 
+    ; other arguments
+    mov edx, r15d
+    mov ecx, r14d
+    mov r8d, r13d
+    mov r9d, r12d
     call exchange_puzzles
-    add esp, 8 ; pop first and second puzzle
 
     dec ebx ; i--
     cmp ebx, 1
     jg spread_tiles_loop
 
 spread_tiles_exit:
-    add esp, 16 ; pop other exchange_puzzles arguments
-    pop esi
-    pop ebx
-
-    mov esp, ebp
-    pop ebp
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
     ret
 
 
@@ -91,99 +91,84 @@ spread_tiles_exit:
 ; return value:
 ;	none
 exchange_puzzles:
-    push ebp
-    mov ebp, esp
+    push rbx
+    push r12
+    push r13
 
-    push ebx
-    push esi
-    push edi
+    mov r13d, edx ; save height of one puzzle
 
-    mov eax, [ebp+8] ; load num of first puzzle
-    mov esi, [ebp+24] ; load m
+    mov eax, edi ; load num of first puzzle
     xor edx, edx ; edx = 0
-    div esi ; puzzle_num // m
-    mov esi, [ebp+16] ; (num//m) * height_1_puzzle
-    mul esi
-    mov ecx, [ebp+28] ; load ImgInfo
-    mov esi, [ecx+8] ; load linebytes
-    mul esi ; linebytes * height_1_puzzle * (num//m)
-    mov ebx, eax ; save result
+    div r8d ; puzzle_num // m
+    mul r13d ; (num//m) * height_1_puzzle
+    mov r10d, [r9+8] ; load linebytes
+    mul r10d ; linebytes * height_1_puzzle * (num//m)
+    mov r11d, eax ; save result
 
-    mov eax, [ebp+8] ; load num of first puzzle
-    mov esi, [ebp+24] ; load m
+    mov eax, edi ; load num of first puzzle
     xor edx, edx
-    div esi
+    div r8d
     mov eax, edx ; puzzle_num % m
-    mov esi, [ebp+20] ; load width of one puzzle
-    mul esi ; width_1_puzzle_in_bytes * (num % m)
-    add ebx, eax
+    mul ecx ; width_1_puzzle_in_bytes * (num % m)
+    add r11d, eax
 
     ; the same calculation for second puzzle
-    mov eax, [ebp+12] ; load num of second puzzle
-    mov esi, [ebp+24] ; load m
+    mov eax, esi ; load num of second puzzle
     xor edx, edx ; edx = 0
-    div esi ; puzzle_num // m
-    mov esi, [ebp+16] ; (num//m) * height_1_puzzle
-    mul esi
-    mov esi, [ecx+8] ; load linebytes
-    mul esi
-    mov esi, eax ; save result
+    div r8d ; puzzle_num // m
+    mul r13d ; (num//m) * height_1_puzzle
+    mov r10d, [r9+8]
+    mul r10d
+    mov r12d, eax ; save result
 
-    mov eax, [ebp+12] ; load num of second puzzle
-    mov edi, [ebp+24] ; load m
+    mov eax, esi ; load num of second puzzle
     xor edx, edx ; edx = 0
-    div edi ; puzzle_num % m
+    div r8d ; puzzle_num % m
     mov eax, edx
-    mov edi, [ebp+20] ; load width of one puzzle
-    mul edi ; width_1_puzzle_in_bytes * (num % m)
-    add esi, eax
+    mul ecx ; width_1_puzzle_in_bytes * (num % m)
+    add r12d, eax
 
-    mov ecx, [ecx+12] ; load pImg
-    add ebx, ecx ; address = base + offset
-    add esi, ecx ; address = base + offset
+    mov rbx, [r9+12] ; load pImg
+    add r11, rbx ; address = base + offset
+    add r12, rbx ; address = base + offset
 
-    mov ecx, [ebp+16] ; init height counter
-    mov edi, [ebp+20] ; init width counter
+    mov r8d, edx ; init height counter
+    mov r13d, ecx ; init width counter
 
-; ebx - address of current pixel of first puzzle
-; esi - address of current pixel of first puzzle
-; ecx - height counter (in pixels)
-; edi - width counter (in bytes)
+; r11 - address of current pixel of first puzzle
+; r12 - address of current pixel of second puzzle
+; r8d - height counter (in pixels)
+; r13d - width counter (in bytes)
 
 exchange_line:
-    mov edi, [ebp+20] ; reset width counter
+    mov r13d, ecx ; reset width counter
 
 exchange_byte:
     ; exchange bytes
-    mov al, [ebx]
-    mov dl, [esi]
-    mov [ebx], dl
-    mov [esi], al
+    mov al, [r11]
+    mov dl, [r12]
+    mov [r11], dl
+    mov [r12], al
 
     ; update addresses
-    inc ebx
-    inc esi
+    inc r11
+    inc r12
 
-    dec edi ; update width counter
+    dec r13d ; update width counter
     jg exchange_byte
 
-    mov eax, [ebp+28] ; load ImgInfo
-    mov edx, [eax+8] ; load linebytes
-    add ebx, edx
-    add esi, edx
+    mov edx, [r9+8] ; load linebytes
+    add r11, edx
+    add r12, edx
 
-    mov edx, [ebp+20] ; load width of one puzzle
-    sub ebx, edx
-    sub esi, edx
+    sub r11, ecx
+    sub r12, ecx
 
-    dec ecx ; update height counter
+    dec r8d ; update height counter
     jg exchange_line
 
 exchange_puzzles_exit:
-    pop edi
-    pop esi
-    pop ebx
-
-    mov esp, ebp
-    pop ebp
+    pop r13
+    pop r12
+    pop rbx
     ret
